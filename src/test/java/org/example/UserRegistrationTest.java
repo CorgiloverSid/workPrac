691fe67836f081c181ac32eaf3dad4de338b4455
u.7297377-1 <siddhantlimbu15@hotmail.co.uk>
2025-02-19 10:05:36 +0000

    servlet practice: + working UserRegistration servlet

---
+++ src/test/java/org/example/UserRegistrationTest.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class UserRegistrationTest extends TestCase {
+
+    UserRegistration usr = new UserRegistration();
+
+
+}
